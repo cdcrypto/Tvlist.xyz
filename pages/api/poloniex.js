96b951ee74b79914c71887926f63cf678144f1ad
cdcrypto <74511588+cdcrypto@users.noreply.github.com>
2024-09-28 21:46:55 -0300

    shit

--- pages/api/poloniex.js
+++ pages/api/poloniex.js
@@ -8,6 +8,7 @@ export default async function handler(req, res) {
     const data = await response.json();
     res.status(200).json(data);
   } catch (error) {
+    console.error('Error fetching data from Poloniex:', error);
     res.status(500).json({ error: 'Error fetching data from Poloniex' });
   }
 }
